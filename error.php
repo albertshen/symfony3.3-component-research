5768ee1e352c15d40c92ee7de099a747d26294fb
Albert Shen <albertshen1206@gmail.com>
2017-11-23 17:13:37 +0800

    i

--- error.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$id = '1111'; 
-$normalizedId = '33333';
-
-trigger_error(sprintf('Service identifiers will be made case sensitive in Symfony 4.0. Using "%s" instead of "%s" is deprecated since version 3.3.', $id, $normalizedId), E_USER_NOTICE);
-
-var_dump(3333);
